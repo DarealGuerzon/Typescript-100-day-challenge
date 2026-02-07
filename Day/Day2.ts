59de5a6ea19fb5dbb5e987884603f00e47886e3a
Daryl Guerzon <guerzondaryl25@gmail.com>
2026-02-07 23:14:50 +0800

    day 2 of coding challenge

---
+++ Day/Day2.ts
@@ -0,0 +1,10 @@
+/*Task: Store your age in a variable and print a message using it.
+Rules: TypeScript only, number type, console.log, ❌ any
+Done When: Compiles, prints correct message*/
+
+let age: number = 22;
+
+console.log(`I am ${age} years old.`);
+
+/*day 2 na commit, medyo nagegets na ang declare at print.*/
+
