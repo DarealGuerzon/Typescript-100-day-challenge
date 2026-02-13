80d9a946a7de823f5aa17b79bf772d52d7ac2984
Daryl Guerzon <guerzondaryl25@gmail.com>
2026-02-13 23:40:16 +0800

    Readonly function

--- Day/Day6.ts
+++ Day/Day6.ts
@@ -1,3 +1,17 @@
 // Task: Make the user.name property readonly and try to change it (observe error).
 // Rules: TypeScript only, readonly, ❌ any, ❌ libraries
 // Done When: Compiler shows error when changing name
+type User = {
+    readonly name: string;
+    age: number;
+};
+
+const user: User ={
+    name: "Daryl",
+    age: 22,
+};
+
+user.name = "Marga";
+
+console.log(user.name);
+console.log(user.age);
