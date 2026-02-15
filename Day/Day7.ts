86f7702f356657d0d1600f25c7604638ab86ca68
Daryl Guerzon <guerzondaryl25@gmail.com>
2026-02-15 23:40:17 +0800

    learned about functions and constants

--- Day/Day7.ts
+++ Day/Day7.ts
@@ -1,3 +1,15 @@
 // Task: Write a function createUser(name, age) that returns a user object with isActive true.
 // Rules: TypeScript only, strict types, ❌ any
 // Done When: Function returns correct object and compiles
+function createUser(name: string, age: number){
+    return{
+        name: name,
+        age: age,
+        isActive:true
+    };
+}
+const user = createUser("Daryl",22);
+console.log(user);
+
+//function makes it dynamic, basically blueprint or acceptable parameters that is reusable
+//const is for making the variable fixed / set
